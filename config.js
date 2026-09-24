d87b6c72b4147e47cde159e1943b1b2f2a6ae481
dipyadav1997-svg <dipyadav1997@gmail.com>
2026-09-24 16:44:57 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// ========================================
+// ASPURA SARPANCH PORTAL
+// SUPABASE CONFIGURATION
+// ========================================
+
+const SUPABASE_URL = "https://kcltcpmqcgrncvpdstzg.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_QCteCVB_hZYTqekE3_NR6g_sjX9787r";
